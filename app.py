eb745694474aaa00ff62123c96682c128a70f094
João Emanuel <jao.emanuel@gmail.com>
2024-10-07 16:36:45 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException, Response
+from pydantic import BaseModel
+from typing import List, Optional
+from cuid import cuid
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    id: Optional[str] = None
+    name: str
+
+
+items: List[Item] = []
+
+
+@app.post("/item")
+def create_item(item: Item):
+    exists = any(item.name == existing.name for existing in items)
+
+    if exists:
+        raise HTTPException(409, "Item already exists!")
+
+    item.id = cuid()
+
+    items.append(item)
+
+    return Response(status_code=200)
+
+@app.get("/item")
+def get_all():
+    return items
+
+
+@app.get("/item/{id}")
+def get_item(id: Optional[str]):
+    print(id)
+    exists = [id == existing.id for existing in items]
+
+    if not any(exists):
+        raise HTTPException(404, "Item not found!")
+    
+    return next(existing for existing in items if id == existing.id)
