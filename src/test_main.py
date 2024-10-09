30f98f1f056dcec5d376f7fdb1f9c9b89567032f
Anthon Hax <51837231+anthonphax@users.noreply.github.com>
2024-10-09 17:35:54 -0300

    deploy step

--- src/test_main.py
+++ src/test_main.py
@@ -18,7 +18,7 @@ def test_get_all_items():
     assert isinstance(response.json(), list)
 
 def test_get_item_by_id():
-    response = client.post("/item", json={"name": "TestItem2"})
+    response = client.post("/item", json={"name": "TestItem1"})
     assert response.status_code == 200
     
     created_item = client.get("/item").json()[0]  
@@ -26,7 +26,7 @@ def test_get_item_by_id():
     
     response = client.get(f"/item/{item_id}")
     assert response.status_code == 200
-    assert response.json()["name"] == "TestItem2"
+    assert response.json()["name"] == "TestItem1"
 
 def test_item_not_found():
     response = client.get("/item/invalid-id")
